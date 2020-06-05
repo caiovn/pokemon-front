0a8e90f95047052b165f331d76ce4f7da5769b24
caiovn <caiovnasci@gmail.com>
2020-06-05 15:52:09 -0300

    - button to randomize made

--- src/pages/Random/index.js
+++ src/pages/Random/index.js
@@ -2,22 +2,32 @@ import React, { useState, useEffect } from 'react';
 
 import api from '../../utils/api';
 
-import { Container } from './style';
+import { Container, RefreshBtn } from './style';
 import PokemonContainer from '../../components/PokemonContainer';
 
 const Random = () => {
   const [data, setData] = useState([]);
+  const [changePokemon, setChangePokemon] = useState(false);
 
   useEffect(() => {
     api.get('api/v1/pokemon/random')
       .then((res) => {
         setData(res.data);
       });
-  }, []);
+  }, [changePokemon]);
+
+  const handleChangePokemon = () => {
+    if (changePokemon === false) {
+      setChangePokemon(true);
+    } else {
+      setChangePokemon(false);
+    }
+  };
 
   return (
     <Container>
       <PokemonContainer pokemon={data} />
+      <RefreshBtn onClick={handleChangePokemon}>Randomize</RefreshBtn>
     </Container>
   );
 };
--- src/pages/Random/style.js
+++ src/pages/Random/style.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin: 15px;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const RefreshBtn = styled.button`
+    background-color: #f7f7f7;
+    margin: 0 auto;
+    padding: 7px 110px;
+    border: 1px solid #2b2d42;
+    border-radius: 4px;
+    font-family: "pokemon font", monospace;
+    font-size: 15px;
 `;
 
 export const OnlyToExcludeError = styled.div``;
